89b5ac590c06c2c3a545fe8e2adb9034532f4792
udhayakumar <udhayakumar.dhanabalan@gmail.com>
2022-06-01 22:04:48 -0700

    migration down added

--- db/migrate/20220602041628_change_fields_to_longtext.rb
+++ db/migrate/20220602041628_change_fields_to_longtext.rb
@@ -1,5 +1,5 @@
 class ChangeFieldsToLongtext < ActiveRecord::Migration[5.2]
-  def change
+  def up
   	change_column :sektor_details, :title, :longtext
   	change_column :sektor_details, :short_description, :longtext
   	change_column :sektor_details, :specs_html, :longtext
@@ -13,5 +13,20 @@ class ChangeFieldsToLongtext < ActiveRecord::Migration[5.2]
   	change_column :anywarenz_details, :temp_image, :longtext
   	change_column :anywarenz_details, :description_html, :longtext
   	change_column :anywarenz_details, :description, :longtext
+  end
+  def down
+    change_column :sektor_details, :title, :string
+    change_column :sektor_details, :short_description, :string
+    change_column :sektor_details, :specs_html, :string
+    change_column :sektor_details, :specs, :string
+    change_column :sektor_details, :description_html, :string
+    change_column :sektor_details, :description, :string
+    change_column :sektor_details, :image, :string
+    change_column :sektor_details, :pdfs, :string
+    change_column :sektor_details, :video, :string
+    change_column :anywarenz_details, :title, :string
+    change_column :anywarenz_details, :temp_image, :text
+    change_column :anywarenz_details, :description_html, :text
+    change_column :anywarenz_details, :description, :text
   end
 end
