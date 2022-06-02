5e13c55b4caa455b22ae0c9bab347b971e5ef3bc
rails-udhaya <udhayakumar.dhanabalan@gmail.com>
2022-06-02 21:10:32 +0530

    added playdistribution and cleanup

--- agents/playdistribution/playdistribution_monitors/playdistribution_data_monitor.rb
+++ agents/playdistribution/playdistribution_monitors/playdistribution_data_monitor.rb
@@ -3,18 +3,18 @@ require 'logger'
 
 
 Dir.mkdir("#{File.dirname(__FILE__)}/logs") unless File.directory?("#{File.dirname(__FILE__)}/logs")
-$logger = Logger.new("#{File.dirname(__FILE__)}/logs/anywarenz_data_monitor.log", 'weekly')
+$logger = Logger.new("#{File.dirname(__FILE__)}/logs/playdistribution_data_monitor.log", 'weekly')
 #~ $logger.level = Logger::DEBUG
 $logger.formatter = Logger::Formatter.new
 
 #~ while true do
-pid_status_english = system("ps -aux | grep anywarenz_data_agent.rb | grep -vq grep")
+pid_status_english = system("ps -aux | grep playdistribution_data_agent.rb | grep -vq grep")
 if pid_status_english
   $logger.info ("nothing to do....")
 else
   $logger.info ("Process started....")
-  #~ system("nohup bundle exec /usr/bin/ruby ../anywarenz_data_agent.rb &")
-  system("nohup bundle exec ruby /var/www/AscentApp/current/agents/anywarenz/anywarenz_data_agent.rb -e production &")
+  #~ system("nohup bundle exec /usr/bin/ruby ../playdistribution_data_agent.rb &")
+  system("nohup bundle exec ruby /var/www/AscentApp/current/agents/playdistribution/playdistribution_data_agent.rb -e production &")
 end
 #~ sleep 300
 #~ end
