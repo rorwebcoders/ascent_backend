6788a3665c222030e8dc19194c55817cf2f55a01
SRM-001 <santhoshsrm28@gmail.com>
2022-06-15 18:33:04 +0530

    Added email and password

--- agents/ingram_micro/ingram_micro_data_agent.rb
+++ agents/ingram_micro/ingram_micro_data_agent.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "itctenders8@gmail.com",
-  :password             => "ITCtenders123",
+  :user_name            => "ascentnoreplymailer@gmail.com",
+  :password             => "ddzpftmwzygawrxj",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
--- agents/sektor/sektor_data_agent.rb
+++ agents/sektor/sektor_data_agent.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "",
-  :password             => "",
+  :user_name            => "ascentnoreplymailer@gmail.com",
+  :password             => "ddzpftmwzygawrxj",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
